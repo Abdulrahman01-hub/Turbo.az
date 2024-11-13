2cf5835734233f3f1e7b0ed478df92496e098e33
Abdulrahman01-hub <barbarossa.123456.1@gmail.com>
2024-11-13 19:21:05 +0300

    Update urls.py

--- turbo_az/cars/urls.py
+++ turbo_az/cars/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('search/', views.search_results, name='search_results'),
-    path('car/<int:car_id>/', views.car_detail, name='car_detail'),  # Araba detayları için URL
+    path('car/<int:car_id>/', views.car_detail, name='car_detail'), 
 
 ]
